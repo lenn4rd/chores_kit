a7feaba6853ffca5965318ee1b1a361160b19011
lenn4rd <hi@lenn4rd.io>
2018-04-08 20:28:53 +0200

    Add very basic executable

--- lib/chores_kit/chores.rb
+++ lib/chores_kit/chores.rb
@@ -1,7 +1,29 @@
-module Chores
+class Chores
+  attr_reader :chore
+
   def self.define(name, &block)
     proxy = ChoresKit::DefinitionProxy.new(name)
     proxy.instance_eval(&block) if block_given?
     proxy.chore
+  end
+
+  def initialize(filename)
+    @chore = instance_eval(File.read(filename))
+  end
+
+  def self.load(filename)
+    new(filename).chore
+  end
+
+  def self.load_all
+    tasks = Pathname.glob('tasks/**/*.rb')
+
+    tasks.each do |task_file|
+      load(Pathname.pwd + task_file)
+    end
+  end
+
+  def run
+    @chore.run!
   end
 end
